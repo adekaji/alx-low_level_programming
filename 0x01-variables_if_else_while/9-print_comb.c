783f5031d3c778e7cc7d44e2781f4fbccf3761ab
Godwin Ajiboye <adekaji@gmail.com>
2022-04-09 11:59:14 -0700

    task 9 commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+*/
+
+int main(void)
+
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+
+		if (num == 57)
+		{
+			break;
+		}
+
+		putchar(',');
+		putchar (' ');
+	}
+	
+	putchar('\n');
+return (0);
+}
+
