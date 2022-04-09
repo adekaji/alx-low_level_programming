6bedd1def2f2ace5f34a08c365bde823b89a66d9
Godwin Ajiboye <adekaji@gmail.com>
2022-04-09 11:37:42 -0700

    task 7 commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	char alph;
+
+	for (alph = 122; alph >= 97; alph--)
+	{
+		putchar(alph);
+	}
+
+	putchar('\n');
+
+return (0);
+}
+
