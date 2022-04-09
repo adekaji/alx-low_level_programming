91e529e33c157fb6c0ff44d852a7ce659fbfab79
Godwin Ajiboye <adekaji@gmail.com>
2022-04-08 21:56:47 -0700

    task 4 commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* 
+* Return: Always 0.
+*/
+int main(void)
+{
+char alph;
+
+for (alph = 'a'; alph <= 'z'; alph++)
+{
+if (alph == 'q')
+continue;
+
+else if (alph == 'e')
+continue;
+
+putchar(alph);
+}
+
+putchar('\n');
+
+return (0);
+}
