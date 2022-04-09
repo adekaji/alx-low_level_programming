67432f92f310ad83803a23963e2c6f32b48f57e4
Godwin Ajiboye <adekaji@gmail.com>
2022-04-09 11:19:25 -0700

    task 6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * Main - Entr point
+  *
+  * Return: Always 0
+  */
+int main(void)
+
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar (i);
+	}
+
+	putchar ('\n');
+
+return (0);
+}
+
