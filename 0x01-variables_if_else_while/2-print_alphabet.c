7c2c948ae45675a57e999cf7b4d490f89287b2a5
Godwin Ajiboye <adekaji@gmail.com>
2022-04-08 20:37:22 -0700

    task 2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success/correct)
+*/
+
+int main(void)
+{
+	char alph;
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+	purchar('\n');
+return (0)
+}
