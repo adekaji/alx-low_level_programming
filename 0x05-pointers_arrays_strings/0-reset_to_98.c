976923ace092da1120445d78cf04328813bd180b
Godwin Ajiboye <adekaji@gmail.com>
2022-04-20 11:38:18 -0700

    commit for pointers and strings readme

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+* reset_to_98 -updates an integer value to 98
+* On: A pointer to an int that will be updated
+*
+* Return: void that means is correct
+*/
+
+void reset_to_98(int *n)
+
+{
+*n = 98;
+}
