b7d1b8a7d232a67e1328e7c2b1062a80bb0689ea
Godwin Ajiboye <adekaji@gmail.com>
2022-04-20 11:47:40 -0700

    commit for pointers and strings readme

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
 * reset_to_98 - updates an integer value to 98
-* On: A pointer to an int that will be updated
+* @n: A pointer to an int that will be updated
 *
 * Return: void that means is correct
 */
