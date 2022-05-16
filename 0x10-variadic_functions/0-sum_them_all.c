a6a05b4d0ec07e73dbd4fc7ffc787947788ab39f
Godwin Ajiboye <adekaji@gmail.com>
2022-05-16 15:44:13 -0700

    commit for task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,7 +10,7 @@
  * sum_them_all - returns the sum of all its parameters.
  * @n: points to the prameters.
  *
- * Return: 0.
+ * Return: If n == 0 - 0.
  */
 
 int sum_them_all(const unsigned int n, ...)
