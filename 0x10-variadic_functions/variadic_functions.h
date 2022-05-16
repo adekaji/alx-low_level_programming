2ef047d4f11a10aa588694557a39cc8b23561456
Godwin Ajiboye <adekaji@gmail.com>
2022-05-16 15:17:48 -0700

    header and readme files

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,18 @@
+/**
+* variadic function code by Godwin Ajiboye
+* header file
+*
+*/
+
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
