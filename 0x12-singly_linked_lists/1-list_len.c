1e76002ed5f64a527850f140e3a06168233257c2
Godwin Ajiboye <adekaji@gmail.com>
2022-05-25 09:21:03 -0700

    removed spaces

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,13 +7,13 @@
 
 size_t list_len(const list_t *h)
 {
-    size_t nelem;
-    nelem = 0;
+	size_t nelem;
+	nelem = 0;
 
-    while (h != NULL)
-    {
-	    h = h->next;
-	    nelem++;
-    }
-    return (nelem);
+	while (h != NULL)
+	{
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
 }
