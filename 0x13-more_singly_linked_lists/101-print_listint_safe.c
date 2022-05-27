1efffd3a668b43276ec57b19c38c43c37875ce55
Godwin Ajiboye <adekaji@gmail.com>
2022-05-26 22:34:37 -0700

    adv task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,24 +6,30 @@
  *
  * Return: number of nodes in the list
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t num = 0;
-    long int diff;
+size_t num = 0;
 
-    while (head)
-    {
-        diff = head - head->next;
-        num++;
-        printf("[%p] %d\n", (void *)head, head->n);
-        if (diff > 0)
-            head = head->next;
-        else
-        {
-            printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-            break;
-        }
-    }
+long int diff;
 
-    return (num);
+while (head)
+{
+diff = head - head->next;
+
+num++;
+printf("[%p] %d\n", (void *)head, head->n);
+
+if (diff > 0)
+head = head->next;
+
+else
+
+{
+printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+break;
+}
+}
+
+return (num);
 }
