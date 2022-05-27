b2f8e79a24532caa73bbbbb8eb0c0287a6896367
Godwin Ajiboye <adekaji@gmail.com>
2022-05-26 22:24:08 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,49 +11,45 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *new;
-	listint_t *temp = *head;
+unsigned int i;
+listint_t *new;
+listint_t *temp = *head;
 
-	new = malloc(sizeof(listint_t));
+new = malloc(sizeof(listint_t));
 
-	if (!new || !head)
+if (!new || !head)
 
-		return (NULL);
+return (NULL);
 
-	new->n = n;
-	new->next = NULL;
+new->n = n;
+new->next = NULL;
 
-	if (idx == 0)
-	{
-		new->next = *head;
+if (idx == 0)
 
-		*head = new;
+{
+new->next = *head;
+*head = new;
 
-		return (new);
+return (new);
 
-	}
+}
 
-	for (i = 0; temp && i < idx; i++)
+for (i = 0; temp && i < idx; i++)
 
-	{
+{
 
-		if (i == idx - 1)
+if (i == idx - 1)
+{
+new->next = temp->next;
+temp->next = new;
 
-		{
-			new->next = temp->next;
+return (new);
+}
 
-			temp->next = new;
+else
+temp = temp->next;
 
-			return (new);
-		}
+}
 
-		else
-
-			temp = temp->next;
-
-	}
-
-
-	return (NULL);
+return (NULL);
 }
