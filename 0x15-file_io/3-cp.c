01bcdc9fc4871538d22866be5f8baf412c1cbbcb
Godwin Ajiboye <adekaji@gmail.com>
2022-06-01 20:41:34 -0700

    commit for task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,53 +9,53 @@
 
 int main(int argc, char **argv)
 {
-	int a;
-	int b;
-	int n;
-	char buf[1024];
+int a;
+int b;
+int n;
+char buf[1024];
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
 
-	a = open(argv[1], O_RDONLY);
+a = open(argv[1], O_RDONLY);
 
-	if (a == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+if (a == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
 
-	b = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+b = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 
-	while ((n = read(a, buf, 1024)) > 0)
-	{
-		if (write(b, buf, n) != n || b == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
+while ((n = read(a, buf, 1024)) > 0)
+{
+if (write(b, buf, n) != n || b == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+}
 
-	if (n == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+if (n == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
 
-	if (close(a) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", a);
-		exit(100);
-	}
+if (close(a) < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", a);
+exit(100);
+}
 
-	if (close(b) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", b);
-		exit(100);
-	}
+if (close(b) < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", b);
+exit(100);
+}
 
-	return (0);
+return (0);
 }
