3ab552e121485f869f1da330c8336ad256de8925
Godwin Ajiboye <adekaji@gmail.com>
2022-06-01 19:49:30 -0700

    commit for task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i]; i++);
+		for (i = 0; text_content[i]; i++)
+			;
 
 		fwrite = write(file, text_content, i);
 
