db0581c4b85ab36a317e9d69f35cc28a718a0864
Godwin Ajiboye <adekaji@gmail.com>
2022-06-01 20:21:04 -0700

    commit for task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,9 @@
 
 int main(int argc, char **argv)
 {
-	int f1, f2, n;
+	int a;
+	int b;
+	int n;
 	char buf[1024];
 
 	if (argc != 3)
@@ -18,19 +20,19 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 
-	f1 = open(argv[1], O_RDONLY);
+	a = open(argv[1], O_RDONLY);
 
-	if (f1 == -1)
+	if (a == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	f2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	b = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 
-	while ((n = read(f1, buf, 1024)) > 0)
+	while ((n = read(a, buf, 1024)) > 0)
 	{
-		if (write(f2, buf, n) != n || f2 == -1)
+		if (write(b, buf, n) != n || b == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
@@ -43,15 +45,15 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	if (close(f1) < 0)
+	if (close(a) < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", a);
 		exit(100);
 	}
 
-	if (close(f2) < 0)
+	if (close(b) < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", b);
 		exit(100);
 	}
 
