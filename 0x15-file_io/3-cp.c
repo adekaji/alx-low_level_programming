3182fba36861ebd3a86a237a30dcc176b8c1c928
Godwin Ajiboye <adekaji@gmail.com>
2022-06-04 14:52:29 -0700

    betty coding style line reduction fix for 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,14 +18,12 @@ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 a = open(argv[1], O_RDONLY);
-
 if (a == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 b = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-
 while ((n = read(a, buf, 1024)) > 0)
 {
 if (write(b, buf, n) != n || b == -1)
@@ -45,8 +43,7 @@ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", a);
 exit(100);
 }
 if (close(b) < 0)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", b);
+{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", b);
 exit(100);
 }
 return (0);
